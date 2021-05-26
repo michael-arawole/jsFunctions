73e72c1f3d29358a58f3862d5e018d162071375d
Michael Arawole <39175160+michael-arawole@users.noreply.github.com>
2021-05-26 19:49:58 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -84,3 +84,45 @@ function redirect(url, reload = false) {
         window.location.reload(true);
 	}
 }
+
+// round up numbers to 2 decimanl places (https://css-tricks.com/snippets/javascript/format-currency/)
+function CurrencyFormatted(amount) {
+	var i = parseFloat(amount);
+	if(isNaN(i)) { i = 0.00; }
+	var minus = '';
+	if(i < 0) { minus = '-'; }
+	i = Math.abs(i);
+	i = parseInt((i + .005) * 100);
+	i = i / 100;
+	s = new String(i);
+	if(s.indexOf('.') < 0) { s += '.00'; }
+	if(s.indexOf('.') == (s.length - 2)) { s += '0'; }
+	s = minus + s;
+	return s;
+}
+
+// properly comma separate number (https://css-tricks.com/snippets/javascript/comma-values-in-numbers/)
+function CommaFormatted(amount) {
+	var delimiter = ","; // replace comma if desired
+	var a = amount.split('.',2)
+	var d = a[1];
+	var i = parseInt(a[0]);
+	if(isNaN(i)) { return ''; }
+	var minus = '';
+	if(i < 0) { minus = '-'; }
+	i = Math.abs(i);
+	var n = new String(i);
+	var a = [];
+	while(n.length > 3) {
+		var nn = n.substr(n.length-3);
+		a.unshift(nn);
+		n = n.substr(0,n.length-3);
+	}
+	if(n.length > 0) { a.unshift(n); }
+	n = a.join(delimiter);
+	if(d.length < 1) { amount = n; }
+	else { amount = n + '.' + d; }
+	amount = minus + amount;
+	return amount;
+}
+
